c006cd072d5394c52266774c053f4d8232c8419e
Sorina Chirilă <chirila_sorina@yahoo.com>
2019-03-04 18:57:27 +0200

    Update IvoryRealTower.java

--- src/main/java/structural/proxy/course/IvoryRealTower.java
+++ src/main/java/structural/proxy/course/IvoryRealTower.java
@@ -1,6 +1,6 @@
 package structural.proxy.course;
 
-public class IvoryRealTower implements WizardTour {
+public class IvoryRealTower implements WizardTower {
     public void enter(Wizard wizard) {
         System.out.println(wizard + " enters the tower...");
     }
